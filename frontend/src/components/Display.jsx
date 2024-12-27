668492de5c35caae58cc2040a8221eb7a9c5e320
Shaheer-developer <m.shaheer062006@gmail.com>
2024-12-28 01:50:46 +0500

    added the forward and backward button functionality , dynamic bg on different album routes

--- frontend/src/components/Display.jsx
+++ frontend/src/components/Display.jsx
@@ -1,11 +1,28 @@
-import React from "react";
-import {Route, Routes} from 'react-router-dom'
+import React, { useEffect, useRef } from "react";
+import {Route, Routes, useLocation} from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
+import { albumsData } from "../assets/frontend-assets/assets.js";
 
 const Display = () => {
+    const displayRef = useRef();
+    const location = useLocation();
+    const isalbum = location.pathname.includes("album")
+    const albumid = isalbum?location.pathname.slice(-1):"";
+    const bgcolor = albumsData[Number(albumid)].bgColor;
+
+    useEffect(()=>{
+        if(isalbum){
+            displayRef.current.style.background = `linear-gradient(${bgcolor},#121212)`
+        }
+        else{
+             displayRef.current.style.background = `#121212`
+        }
+
+    },[location.pathname])
+
     return(
-        <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+        <div ref={displayRef} className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
             <Routes>
                 <Route path='/' element={<DisplayHome/>}/>
                 <Route path='/album/:id' element={<DisplayAlbum/>}/>
